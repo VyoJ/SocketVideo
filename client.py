229aff2428d0b55a28896b3b8f4414395246e477
Vrashank Shetty <vrashankshetty.s@gmail.com>
2024-02-22 13:08:48 +0530

    feb22

--- client.py
+++ client.py
@@ -1,25 +1,85 @@
-import socket,cv2,pickle,struct
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = '172.20.10.2' 
-port = 9999
-client_socket.connect((host_ip,port))
-data = b""
-payload_size = struct.calcsize("Q")
-while True:
-	while len(data) < payload_size:
-		packet = client_socket.recv(4*1024) 
-		if not packet: break
-		data+=packet
-	packed_msg_size = data[:payload_size]
-	data = data[payload_size:]
-	msg_size = struct.unpack("Q",packed_msg_size)[0]
+# import socket,cv2,pickle,struct
+# client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# host_ip = "192.168.1.7"
+# port = 9222
+# client_socket.connect((host_ip,port))
+# data = b""
+# payload_size = struct.calcsize("Q")
+# while True:
+# 	while len(data) < payload_size:
+# 		packet = client_socket.recv(8*1024) 
+# 		if not packet: break
+# 		data+=packet
+# 	packed_msg_size = data[:payload_size]
+# 	data = data[payload_size:]
+# 	msg_size = struct.unpack("Q",packed_msg_size)[0]
 	
-	while len(data) < msg_size:
-		data += client_socket.recv(4*1024)
-	frame_data = data[:msg_size]
-	data  = data[msg_size:]
-	frame = pickle.loads(frame_data)
-	cv2.imshow("RECEIVING VIDEO",frame)
-	if cv2.waitKey(1) == '13':
-		break
-client_socket.close()
+# 	while len(data) < msg_size:
+# 		data += client_socket.recv(8*1024)
+# 	frame_data = data[:msg_size]
+# 	data  = data[msg_size:]
+# 	frame = pickle.loads(frame_data)
+# 	cv2.imshow("RECEIVING VIDEO",frame)
+# 	if cv2.waitKey(1) == '13':
+# 		break
+# client_socket.close()
+
+import socket, cv2, pickle, struct
+import threading
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = "192.168.1.7"
+port =  9111
+client_socket.connect((host_ip, port))
+
+def send_frames(client_socket):
+    vid = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    while vid.isOpened():
+        ret, frame = vid.read()
+        if not ret:
+            break
+        frame = cv2.resize(frame, (320,  240))
+        a = pickle.dumps(frame)
+        message = struct.pack("Q", len(a)) + a
+        client_socket.sendall(message)
+        cv2.imshow('TRANSMITTING VIDEO', frame)
+        if cv2.waitKey(1) &  0xFF == ord('q'):
+            break
+    client_socket.close()
+
+def receive_frames(client_socket):
+    while True:
+        data = b""
+        payload_size = struct.calcsize("Q")
+        while len(data) < payload_size:
+            try:
+                packet = client_socket.recv(4096)
+            except OSError as e:
+                if e.errno ==  9:  # Bad file descriptor
+                    print("Socket is not open or already closed.")
+                else:
+                    print(f"Unexpected error: {e}")
+            data += packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+        while len(data) < msg_size:
+            data += client_socket.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        cv2.imshow("RECEIVING VIDEO", frame)
+        if cv2.waitKey(1) &  0xFF == ord('q'):
+            break
+    client_socket.close()
+
+# Create threads for sending and receiving frames
+send_thread = threading.Thread(target=send_frames, args=(client_socket,))
+receive_thread = threading.Thread(target=receive_frames, args=(client_socket,))
+
+send_thread.start()
+receive_thread.start()
+
+send_thread.join()
+receive_thread.join()
--- server.py
+++ server.py
@@ -1,32 +1,77 @@
-import socket, cv2, pickle, struct, imutils
+# import socket, cv2, pickle, struct, imutils
 
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_name  = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-print('HOST IP:',host_ip)
-port = 9999
-socket_address = (host_ip,port)
+# server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# host_name  = socket.gethostname()
+# host_ip = "192.168.1.7"
+# print('HOST IP:',host_ip)
+# port = 9111
+# socket_address = (host_ip,port)
+
+# server_socket.bind(socket_address)
+
+# # Socket Listen
+# server_socket.listen(5)
+# print("LISTENING AT:",socket_address)
+
+# # Socket Accept
+# while True:
+# 	client_socket,addr = server_socket.accept()
+# 	print('GOT CONNECTION FROM:',addr)
+# 	if client_socket:
+# 		vid = cv2.VideoCapture(0)
+		
+# 		while(vid.isOpened()):
+# 			img,frame = vid.read()
+# 			frame = imutils.resize(frame,width=320)
+# 			a = pickle.dumps(frame)
+# 			message = struct.pack("Q",len(a))+a
+# 			client_socket.sendall(message)
+			
+# 			cv2.imshow('TRANSMITTING VIDEO',frame)
+# 			if cv2.waitKey(1) == '13':
+# 				client_socket.close()
+
+import socket, cv2, pickle, struct, imutils
+import threading
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = "192.168.1.7"
+port =  9111
+socket_address = (host_ip, port)
 
 server_socket.bind(socket_address)
+server_socket.listen(2)  # Listen for  2 clients
+print("LISTENING AT:", socket_address)
 
-# Socket Listen
-server_socket.listen(5)
-print("LISTENING AT:",socket_address)
+clients = []
+while len(clients) <  2:
+    client_socket, addr = server_socket.accept()
+    print('GOT CONNECTION FROM:', addr)
+    clients.append(client_socket)
 
-# Socket Accept
-while True:
-	client_socket,addr = server_socket.accept()
-	print('GOT CONNECTION FROM:',addr)
-	if client_socket:
-		vid = cv2.VideoCapture(0)
-		
-		while(vid.isOpened()):
-			img,frame = vid.read()
-			frame = imutils.resize(frame,width=320)
-			a = pickle.dumps(frame)
-			message = struct.pack("Q",len(a))+a
-			client_socket.sendall(message)
-			
-			cv2.imshow('TRANSMITTING VIDEO',frame)
-			if cv2.waitKey(1) == '13':
-				client_socket.close()
+def relay_frames(client_socket):
+    while True:
+        data = b""
+        payload_size = struct.calcsize("Q")
+        while len(data) < payload_size:
+            packet = client_socket.recv(4096)
+            if not packet: break
+            data += packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
+
+        while len(data) < msg_size:
+            data += client_socket.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame = pickle.loads(frame_data)
+
+        # Send the frame to the other client
+        other_client = clients[0] if client_socket is clients[1] else clients[1]
+        other_client.sendall(frame_data)
+
+# Start relaying frames for each client
+for client_socket in clients:
+    client_thread = threading.Thread(target=relay_frames, args=(client_socket,))
+    client_thread.start()
